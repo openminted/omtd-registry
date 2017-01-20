09affbee8a7549dea832724ddf82bdc96f0cb1c8
Antonis Lempesis <antleb@di.uoa.gr>
2017-01-20 19:34:06 +0200

    added corpus upload/download

--- src/main/java/eu/openminted/registry/service/CorpusController.java
+++ src/main/java/eu/openminted/registry/service/CorpusController.java
@@ -3,20 +3,25 @@ package eu.openminted.registry.service;
 import eu.openminted.registry.domain.Corpus;
 import eu.openminted.registry.exception.ResourceNotFoundException;
 import eu.openminted.registry.exception.ServerError;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/request/corpus")
 public class CorpusController {
 
     @Autowired
-    ResourceCRUDService<Corpus> corpusService;
+        CorpusService corpusService;
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ResourceNotFoundException.class)
@@ -62,7 +67,24 @@ public class CorpusController {
 
         corpusService.delete(corpus);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
 
+    @RequestMapping(value = "upload", method = RequestMethod.POST)
+    public ResponseEntity<String> uploadCorpus(@RequestParam("filename") String filename, @RequestParam("file") MultipartFile file) {
 
+        try {
+            return new ResponseEntity<String>(corpusService.uploadCorpus(filename, file.getInputStream()), HttpStatus.OK);
+        } catch (IOException e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(value = "download", method = RequestMethod.GET)
+    @ResponseBody
+    public void downloadCorpus(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String mimeType = "application/octet-stream";
+
+        response.setContentType(mimeType);
+        IOUtils.copyLarge(corpusService.downloadCorpus(request.getParameter("archiveId")), response.getOutputStream());
     }
 }
