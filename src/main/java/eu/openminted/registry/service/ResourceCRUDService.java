ebbd736a0402660c23df00c66adbfff4ce76fd28
Stefanos Gatsios <stevengatsios@gmail.com>
2017-06-16 12:42:24 +0300

    Update interface to getAll resources

--- src/main/java/eu/openminted/registry/service/ResourceCRUDService.java
+++ src/main/java/eu/openminted/registry/service/ResourceCRUDService.java
@@ -1,11 +1,15 @@
 package eu.openminted.registry.service;
 
+import java.util.List;
+
 /**
  * Created by stefanos on 15-Nov-16.
  */
 public interface ResourceCRUDService<T> {
 
     T get(String id);
+
+    List<T> getAll();
 
     void add(T resource);
 
