c09254da0b6803efd02a9b82507a904366258797
Stefanos Gatsios <stevengatsios@gmail.com>
2017-06-23 18:03:15 +0300

    Created metadata generator

--- src/main/java/eu/openminted/registry/generate/MetadataHeaderInfoGenerate.java
+++ src/main/java/eu/openminted/registry/generate/MetadataHeaderInfoGenerate.java
@@ -2,6 +2,8 @@ package eu.openminted.registry.generate;
 
 import eu.openminted.registry.domain.*;
 import org.mitre.openid.connect.model.OIDCAuthenticationToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.xml.datatype.DatatypeConfigurationException;
@@ -16,7 +18,11 @@ import java.util.UUID;
  */
 public class MetadataHeaderInfoGenerate {
 
-    static public MetadataHeaderInfo generate() throws DatatypeConfigurationException {
+    private static final String SCOPE_AFFILIATION = "edu_person_scoped_affiliations";
+
+    private static Logger logger = LoggerFactory.getLogger(MetadataHeaderInfoGenerate.class);
+
+    static public MetadataHeaderInfo generate(){
         MetadataHeaderInfo info = new MetadataHeaderInfo();
 
         //
@@ -24,7 +30,12 @@ public class MetadataHeaderInfoGenerate {
         //
         GregorianCalendar gregory = new GregorianCalendar();
         gregory.setTime(new Date());
-        XMLGregorianCalendar calendar = DatatypeFactory.newInstance().newXMLGregorianCalendar(gregory);
+        XMLGregorianCalendar calendar = null;
+        try {
+            calendar = DatatypeFactory.newInstance().newXMLGregorianCalendar(gregory);
+        } catch (DatatypeConfigurationException e) {
+            e.printStackTrace();
+        }
         info.setMetadataCreationDate(calendar);
         info.setMetadataLastDateUpdated(calendar);
 
@@ -43,15 +54,47 @@ public class MetadataHeaderInfoGenerate {
         OIDCAuthenticationToken authentication = (OIDCAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
 
         PersonInfo personInfo = new PersonInfo();
-        Name name = new Name();
-        name.setValue(authentication.getName());
-        name.setLang("en");
-        personInfo.getNames().add(name);
+        SeparateNames name = new SeparateNames();
+        GivenName givenName = new GivenName();
+        givenName.setValue(authentication.getUserInfo().getGivenName());
+        Surname surname = new Surname();
+        surname.setValue(authentication.getUserInfo().getFamilyName());
+
+        name.getGivenNames().add(givenName);
+        name.getSurnames().add(surname);
+
+        personInfo.setSeparateNames(name);
 
         PersonIdentifier personIdentifier = new PersonIdentifier();
         personIdentifier.setValue(authentication.getSub());
         personIdentifier.setPersonIdentifierSchemeName(PersonIdentifierSchemeNameEnum.OTHER);
         personInfo.getPersonIdentifiers().add(personIdentifier);
+
+        //
+        // Set affiliations
+        //
+        authentication.getUserInfo().getSource().getAsJsonArray(SCOPE_AFFILIATION).forEach(af -> {
+            String[] organizationPosition = af.getAsString().split("@");
+            if(organizationPosition.length == 2) {
+                Affiliation affiliation = new Affiliation();
+                OrganizationInfo organizationInfo = new OrganizationInfo();
+                OrganizationName organizationName = new OrganizationName();
+                organizationName.setValue(organizationPosition[1]);
+                organizationInfo.getOrganizationNames().add(organizationName);
+                affiliation.setAffiliatedOrganization(organizationInfo);
+                affiliation.setPosition(organizationPosition[0]);
+                personInfo.getAffiliations().add(affiliation);
+            } else {
+                logger.warn("The provided affiliation was not in position@organization format");
+            }
+        });
+
+        //
+        // Set communication info
+        //
+        CommunicationInfo communicationInfo = new CommunicationInfo();
+        communicationInfo.getEmails().add(authentication.getUserInfo().getEmail());
+        personInfo.setCommunicationInfo(communicationInfo);
 
         info.getMetadataCreators().add(personInfo);
         return info;
