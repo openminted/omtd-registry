5da56ad3495f1fb654268e66cd0fa5aaa864c3f0
Stefanos Gatsios <stevengatsios@gmail.com>
2017-08-04 16:58:31 +0300

    Upgrade registry core version 1.3.1
    And added groups

--- src/main/java/eu/openminted/registry/service/RequestServiceImpl.java
+++ src/main/java/eu/openminted/registry/service/RequestServiceImpl.java
@@ -1,18 +1,18 @@
 package eu.openminted.registry.service;
 
-import com.google.common.collect.Lists;
-import eu.openminted.registry.core.domain.*;
+import eu.openminted.registry.core.domain.Browsing;
+import eu.openminted.registry.core.domain.FacetFilter;
 import eu.openminted.registry.core.service.AbstractGenericService;
 import eu.openminted.registry.core.service.SearchService;
 import eu.openminted.registry.core.service.ServiceException;
-import eu.openminted.registry.domain.*;
+import eu.openminted.registry.domain.BaseMetadataRecord;
 import org.apache.log4j.Logger;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.net.UnknownHostException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Service("requestService")
 public class RequestServiceImpl extends AbstractGenericService implements RequestService {
@@ -31,8 +31,12 @@ public class RequestServiceImpl extends AbstractGenericService implements Reques
 
     public Browsing getResponseByFiltersElastic(FacetFilter filter) {
         filter.addFilter("public",true);
-        filter.setBrowseBy(getBrowseBy());
         return getResults(filter);
+    }
+
+    public Map<String,List<?>> getResourceGroupedElastic(FacetFilter filter, String category) throws ServiceException {
+        filter.addFilter("public",true);
+        return getResultsGrouped(filter,category);
     }
 
     @Override
