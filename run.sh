ec154ffcf1c74a2e3b698f4448537409cfadca2d
Katerina Gkirtzou <katerina.gkirtzou@gmail.com>
2017-08-04 18:23:00 +0300

    Adding test publisher for operation messages

---
+++ run.sh
@@ -0,0 +1,16 @@
+# compile project and pack as war 
+mvn clean
+mvn package
+
+# Undeploy previous version
+echo "Undeploy previous version"
+curl "http://test:test@localhost:8080/manager/text/undeploy?path=/omtd-registry"
+
+#../../InstalledTools/apache-tomcat-8.0.45/bin/shutdown.sh
+#cp target/omtd-registry.war ../../InstalledTools/apache-tomcat-8.0.45/webapps/omtd-registry.war
+#../../InstalledTools/apache-tomcat-8.0.45/bin/startup.sh
+
+# Deploy new version 
+echo "Deploy new version"
+curl --upload-file target/omtd-registry.war    "http://test:test@localhost:8080/manager/text/deploy?path=/omtd-registry" 
+
