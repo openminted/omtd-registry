59f9982db49184f7346baac48d47d1a206564a39
Antonis Lempesis <antleb@di.uoa.gr>
2017-09-01 18:14:36 +0300

    added new IncompleteCorpus resourceType

--- src/main/java/eu/openminted/registry/service/IncompleteCorpusService.java
+++ src/main/java/eu/openminted/registry/service/IncompleteCorpusService.java
@@ -8,22 +8,6 @@ import java.io.InputStream;
 /**
  * Created by antleb on 1/19/17.
  */
-public interface CorpusService extends ResourceCRUDService<Corpus> {
-
-    /**
-     * Uploads a zipped corpus and returns the archive id where it was saved.
-     *
-     * @param inputStream
-     * @return
-     */
-    String uploadCorpus(String filename, InputStream inputStream);
-
-    /**
-     * Download the corpus.
-     *
-     * @param archiveId
-     * @return
-     */
-    InputStream downloadCorpus(String archiveId);
+public interface IncompleteCorpusService extends ResourceCRUDService<Corpus> {
 
 }
