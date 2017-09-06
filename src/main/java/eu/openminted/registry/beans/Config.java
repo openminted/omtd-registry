e8ff3eaa3f10cb4377e07ceb6de164ecaf53edbd
Stefanos Gatsios <stevengatsios@gmail.com>
2017-09-06 16:02:39 +0300

    Tralala

--- src/main/java/eu/openminted/registry/beans/Config.java
+++ src/main/java/eu/openminted/registry/beans/Config.java
@@ -1,18 +1,28 @@
 package eu.openminted.registry.beans;
 
+import eu.openminted.registry.core.domain.Facet;
 import eu.openminted.registry.messages.JMSConsumer;
-import org.apache.log4j.Logger;
+import org.apache.activemq.spring.ActiveMQConnectionFactory;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
 
 /**
  * Created by stefanos on 14/6/2017.
@@ -20,10 +30,11 @@ import javax.annotation.PostConstruct;
 
 @Configuration
 @EnableRedisHttpSession
+@EnableJms
 @PropertySource(value = { "classpath:application.properties", "classpath:registry.properties"} )
 public class Config {
 
-    private static Logger logger = Logger.getLogger(Config.class);
+    private static Logger logger = LogManager.getLogger(Config.class);
 
     @Value("${redis.host}")
     private String host;
@@ -34,13 +45,32 @@ public class Config {
     @Value("${redis.password:#{null}}")
     private String password;
 
+    @Value("${jms.host}")
+    private String jmsHost;
+
     @Autowired
     private JMSConsumer consumer;
 
+    @Bean
+    public JmsTemplate jmsTemplate(){
+        JmsTemplate template = new JmsTemplate();
+        template.setConnectionFactory(activeMQConnectionFactory());
+        template.setPubSubDomain(true);
+        template.setMessageConverter(jacksonJmsMessageConverter());
+        return template;
+    }
+
     @PostConstruct
     public void initializeJMSConsumer() {
-        System.out.println("Creating consumer");
-        new Thread(() -> consumer.listen()).start();
+//        System.out.println("Creating consumer");
+//        new Thread(() -> consumer.listen()).start();
+        // Send a message with a POJO - the template reuse the message converter
+        logger.info("Send hello world");
+        try {
+            jmsTemplate().convertAndSend("test", new Facet("hello","world",null));
+        } catch (Exception e) {
+            logger.warn(e);
+        }
     }
 
     @Bean
@@ -52,11 +82,37 @@ public class Config {
     }
 
     @Bean
+    public ActiveMQConnectionFactory activeMQConnectionFactory() {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        connectionFactory.setBrokerURL(jmsHost);
+        connectionFactory.setConnectionIDPrefix("omtd-registry");
+        logger.info("ActiveMQConnection Factory created for " + jmsHost);
+        return connectionFactory;
+    }
+
+    @Bean // Serialize message content to json using TextMessage
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+        DefaultJmsListenerContainerFactory factory
+                = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(activeMQConnectionFactory());
+        factory.setPubSubDomain(true);
+        factory.setMessageConverter(jacksonJmsMessageConverter());
+        return factory;
+    }
+
+    @Bean
     public CookieSerializer cookieSerializer() {
         DefaultCookieSerializer serializer = new DefaultCookieSerializer();
         serializer.setCookieName("SESSION"); // <1>
         serializer.setCookiePath("/"); // <2>
-        // serializer.setDomainNamePattern("^.+?\\.(\\w+\\.[a-z]+)$"); // <3>
         return serializer;
     }
 
--- src/main/java/eu/openminted/registry/messages/JMSConsumer.java
+++ src/main/java/eu/openminted/registry/messages/JMSConsumer.java
@@ -2,11 +2,21 @@ package eu.openminted.registry.messages;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.openminted.corpus.CorpusBuildingState;
+import eu.openminted.corpus.CorpusStatus;
+import eu.openminted.registry.core.domain.Facet;
+import eu.openminted.registry.core.domain.Resource;
+import eu.openminted.registry.core.service.ParserService;
+import eu.openminted.registry.core.service.SearchService;
+import eu.openminted.registry.core.service.ServiceException;
+import eu.openminted.registry.domain.Corpus;
 import eu.openminted.registry.service.CorpusBuildingStateServiceImpl;
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.log4j.Logger;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -14,72 +24,35 @@ import javax.jms.*;
 import java.io.IOException;
 
 @Component("jmsConsumer")
-public class JMSConsumer implements ExceptionListener, MessageListener {
-    private static Logger log = Logger.getLogger(JMSConsumer.class.getName());
+public class JMSConsumer { // implementsExceptionListener, MessageListener
+    private static Logger log = LogManager.getLogger(JMSConsumer.class.getName());
 
-    @org.springframework.beans.factory.annotation.Value("${jms.content.corpus.topic}")
+    @Value("${jms.content.corpus.topic}")
     private String topicName;
 
-    @org.springframework.beans.factory.annotation.Value("${jms.host}")
+    @Value("${jms.host}")
     private String jmsHost;
 
 
-//    @Autowired
-    private ActiveMQConnectionFactory connectionFactory;
+    @Autowired
+    public ParserService parserPool;
 
     @Autowired
     CorpusBuildingStateServiceImpl corpusBuildingStateService;
 
-    @PostConstruct
-    public void init() {
-        connectionFactory =  new ActiveMQConnectionFactory(jmsHost);
-        connectionFactory.setConnectionIDPrefix("omtd-registry");
+    @Autowired
+    IncompleteCorpusServiceImpl incompleteCorpusService;
+
+    @JmsListener(containerFactory = "jmsListenerContainerFactory", destination = "test")
+    public void receiveMessage(Facet message) throws JMSException {
+        log.info(message);
     }
 
-    private Connection connection;
-
-    private Session session;
-
-    public void listen() {
-        try {
-            // Create a Connection
-            connection = connectionFactory.createConnection();
-//            connection = new ActiveMQConnectionFactory(jmsHost).createConnection();
-            connection.setExceptionListener(this);
-
-            // Set unique clientID to connection prior to connect
-            connection.setClientID(Integer.toString(this.hashCode()));
-            connection.start();
 
 
-            // Create a Session
-            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-            // Create the Topic
-            Topic topic = session.createTopic(topicName);
-
-            // Create a TopicSubscriber from the Session to the Topic
-            TopicSubscriber subscriber = session.createDurableSubscriber(topic, "SUBSCRIBER");
-            subscriber.setMessageListener(this);
-
-        } catch (Exception e) {
-            log.error("Caught Exception: " + e);
-        }
-    }
-
-    public synchronized void onException(JMSException ex) {
-        log.error("JMS Exception occurred.  Shutting down client.");
-
-        try {
-            session.close();
-            connection.close();
-        } catch (JMSException e) {
-            log.error("JMS Exception occurred while shutting down client.", e);
-        }
-    }
-
-    @Override
+    @JmsListener(containerFactory = "jmsListenerContainerFactory", destination = "${jms.content.corpus.topic}")
     public void onMessage(Message message) {
+        log.info(message);
         if (message instanceof TextMessage) {
             try {
                 TextMessage textMessage = (TextMessage) message;
