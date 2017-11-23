17b1b97d97a5183706ebf0c317f7809f6a767ea6
John Diplas <jdiplas@gmail.com>
2017-11-23 13:18:51 +0200

    Implemented JMSListener for maven group/artifact/version storing of xml

--- src/main/java/eu/openminted/registry/messages/JMSConsumer.java
+++ src/main/java/eu/openminted/registry/messages/JMSConsumer.java
@@ -5,9 +5,7 @@ import eu.openminted.corpus.CorpusStatus;
 import eu.openminted.registry.core.domain.Resource;
 import eu.openminted.registry.core.service.ParserService;
 import eu.openminted.registry.core.service.SearchService;
-import eu.openminted.registry.domain.ComponentDistributionFormEnum;
-import eu.openminted.registry.domain.ComponentDistributionInfo;
-import eu.openminted.registry.domain.Corpus;
+import eu.openminted.registry.domain.*;
 import eu.openminted.registry.service.CorpusBuildingStateServiceImpl;
 import eu.openminted.registry.service.DockerService;
 import eu.openminted.registry.service.IncompleteCorpusServiceImpl;
@@ -20,9 +18,14 @@ import org.springframework.stereotype.Component;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.TextMessage;
-import javax.xml.soap.Text;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component("jmsConsumer")
 public class JMSConsumer {
@@ -65,8 +68,6 @@ public class JMSConsumer {
     @JmsListener(containerFactory = "jmsTopicListenerContainerFactory", destination = "${jms.component.create.topic:registry.component.create}")
     public void receiveStateTopic(Message message) throws JMSException, UnknownHostException, ExecutionException, InterruptedException {
 
-        log.info("Received component's creation, processing...");
-
         String responseBody = ((TextMessage) message).getText();
         JSONObject object = new JSONObject(responseBody);
 
@@ -85,6 +86,66 @@ public class JMSConsumer {
             dockerService.deleteDockerFlow(url,image_id);
         }
 
+        mavenExportDirectory(resource);
+    }
+
+    @JmsListener(containerFactory = "jmsTopicListenerContainerFactory", destination = "${jms.component.update.topic:registry.component.update}")
+    public void receiveStateTopicUpdated(Message message) throws JMSException, UnknownHostException, ExecutionException, InterruptedException {
+
+        String responseBody = ((TextMessage) message).getText();
+        JSONObject object = new JSONObject(responseBody);
+
+        Resource resource = new Resource();
+        resource.setPayloadFormat("xml");
+        resource.setPayload((String) object.get("resource"));
+
+        mavenExportDirectory(resource);
+
+    }
+
+    private void mavenExportDirectory(Resource resource) throws ExecutionException, InterruptedException {
+        eu.openminted.registry.domain.Component component = parserPool.serialize(resource, eu.openminted.registry.domain.Component.class).get();
+        ResourceIdentifier resourceIdentifier = component.getComponentInfo().getIdentificationInfo().getResourceIdentifiers().get(0);
+
+        if (resourceIdentifier.getResourceIdentifierSchemeName() == ResourceIdentifierSchemeNameEnum.MAVEN) {
+
+            Pattern pattern = Pattern.compile("mvn:([\\w\\._-]+):([\\w\\._-]+):([\\.\\w_-]+)");
+            Matcher matcher = pattern.matcher(resourceIdentifier.getValue());
+            matcher.find();
+
+            String groupId = matcher.group(1);
+            String artifactId = matcher.group(2);
+            String version = matcher.group(3);
+
+            File f = new File("../maven-data/"+groupId+"/"+artifactId+"/"+version+"/");
+
+            if(!f.exists()) {
+                try{
+                    if(f.mkdirs()) {
+                        System.out.println("Directory Created");
+                    } else {
+                        System.out.println("Directory is not created");
+                    }
+                } catch(Exception e){
+                    //  Demo purposes only.  Do NOT do this in real code.  EVER.
+                    //  It squashes exceptions ...
+                    e.printStackTrace();
+                }
+            }
+
+            f = new File("../maven-data/"+groupId+"/"+artifactId+"/"+version+"/"+component.getMetadataHeaderInfo().getMetadataRecordIdentifier().getValue()+".xml");
+
+            try {
+                FileWriter fw = new FileWriter(f.getAbsolutePath());
+                BufferedWriter bw = new BufferedWriter(fw);
+                bw.write(resource.getPayload());
+
+                bw.close();
+                fw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
