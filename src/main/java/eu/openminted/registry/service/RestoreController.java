409e2b0ae2707f113ab02affba1f12d90c43fbd6
John Diplas <jdiplas@gmail.com>
2017-12-12 11:14:53 +0200

    - Added ParsePool deserializeResource and deserializeResourceType - Implemented dump functionality for exporting schema and data for resource types task #551 - Added script for deleting resource types - Added httpResponse to include all the possibles MediaTypes - Added "deleteResourceType" functionality for task #555 - Schema controller now returns content as XML or JSON not as Text - Basic implementation of restore service

---
+++ src/main/java/eu/openminted/registry/service/RestoreController.java
@@ -0,0 +1,21 @@
+package eu.openminted.registry.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class RestoreController {
+
+    @Autowired
+    RestoreService restoreService;
+
+
+    @RequestMapping(value = "/restore/", method = RequestMethod.POST)
+    @ResponseBody
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public void dumpAll(@RequestParam("datafile") MultipartFile file ) {
+        restoreService.restoreDataFromZip(file);
+    }
+}
