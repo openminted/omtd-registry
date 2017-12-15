d3b53db0ef4df4ff03fdd5eb9ffc2f72b04e44af
Stefanos Gatsios <stevengatsios@gmail.com>
2017-12-15 17:33:28 +0200

    Deny application that are not components and the inverse

--- src/main/java/eu/openminted/registry/service/ApplicationServiceImpl.java
+++ src/main/java/eu/openminted/registry/service/ApplicationServiceImpl.java
@@ -1,5 +1,6 @@
 package eu.openminted.registry.service;
 
+import eu.openminted.registry.core.service.ServiceException;
 import eu.openminted.registry.domain.Component;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,22 @@ public class ApplicationServiceImpl extends OmtdGenericService<Component>{
     @Override
     public String getResourceType() {
         return "application";
+    }
+
+    @Override
+    public void add(Component resource) {
+        if(!resource.getComponentInfo().isApplication()){
+            throw new ServiceException("Expected an application not a component");
+        }
+        super.add(resource);
+    }
+
+    @Override
+    public void update(Component newResource) {
+        if(!newResource.getComponentInfo().isApplication()){
+            throw new ServiceException("Cannot update an application to a component");
+        }
+        super.update(newResource);
     }
 
 }
--- src/main/java/eu/openminted/registry/service/ComponentServiceImpl.java
+++ src/main/java/eu/openminted/registry/service/ComponentServiceImpl.java
@@ -1,5 +1,6 @@
 package eu.openminted.registry.service;
 
+import eu.openminted.registry.core.service.ServiceException;
 import eu.openminted.registry.domain.Component;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
@@ -21,4 +22,19 @@ public class ComponentServiceImpl extends OmtdGenericService<Component>{
         return "component";
     }
 
+    @Override
+    public void add(Component resource) {
+        if(resource.getComponentInfo().isApplication()){
+            throw new ServiceException("Expected a component not an application");
+        }
+        super.add(resource);
+    }
+
+    @Override
+    public void update(Component newResource) {
+        if(newResource.getComponentInfo().isApplication()){
+            throw new ServiceException("Cannot update a component to an application");
+        }
+        super.update(newResource);
+    }
 }
