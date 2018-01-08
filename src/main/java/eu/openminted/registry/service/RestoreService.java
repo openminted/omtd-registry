bae3c17996caf4077feb90580f0bf1c27b902215
John Diplas <jdiplas@gmail.com>
2018-01-08 11:52:03 +0200

    Moved restoreService and dumpService functionality to the registry-core level

--- src/main/java/eu/openminted/registry/service/RestoreService.java
+++
@@ -1,8 +0,0 @@
-package eu.openminted.registry.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface RestoreService {
-
-	void restoreDataFromZip(MultipartFile file);
-}
