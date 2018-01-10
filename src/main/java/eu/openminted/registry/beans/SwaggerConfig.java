95e968ea5a4ff4938a1458570c9c64b0f277681f
Stefanos Gatsios <stevengatsios@gmail.com>
2018-01-10 17:19:55 +0200

    Try to fix reverse proxy limitation on swagger

--- src/main/java/eu/openminted/registry/beans/SwaggerConfig.java
+++ src/main/java/eu/openminted/registry/beans/SwaggerConfig.java
@@ -1,20 +1,53 @@
 package eu.openminted.registry.beans;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.servlet.ServletContext;
+
 @Configuration
 @EnableSwagger2
+@PropertySource(value = { "classpath:application.properties", "classpath:registry.properties"} )
 public class SwaggerConfig {
+
+
+    @Value("${openminted.debug:#{false}}")
+    public Boolean isLocalhost;
+
+    @Value("${openminted.path:api}")
+    public String host;
+
+    @Autowired
+    ServletContext context;
+
+    private RelativePathProvider pathProvider() {
+        if(isLocalhost) {
+            return new RelativePathProvider(context);
+        } else {
+            return new RelativePathProvider(context) {
+                @Override
+                protected String applicationPath() {
+                    return "/";
+                }
+            };
+        }
+    }
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .pathProvider(pathProvider())
+                .pathMapping(isLocalhost ? null : host)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("eu.openminted.registry.service"))
                 .paths(PathSelectors.any())
