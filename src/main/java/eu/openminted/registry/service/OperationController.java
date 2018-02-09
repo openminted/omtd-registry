3c4dfa2230769f5b0e5c6243d5d567d47424dab0
Stefanos Gatsios <stevengatsios@gmail.com>
2018-02-09 21:59:33 +0200

    Operation Controller execute job

--- src/main/java/eu/openminted/registry/service/OperationController.java
+++ src/main/java/eu/openminted/registry/service/OperationController.java
@@ -3,7 +3,10 @@ package eu.openminted.registry.service;
 import eu.openminted.registry.core.service.ResourceCRUDService;
 import eu.openminted.registry.domain.operation.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,4 +21,13 @@ public class OperationController extends GenericRestController<Operation>{
     OperationController(ResourceCRUDService<Operation> service) {
         super(service);
     }
+
+    @RequestMapping(value = "executeJob", method = RequestMethod.GET)
+    ResponseEntity<String> executeJob(
+            @RequestParam(value = "corpusId") String corpusId,
+            @RequestParam(value="applicationId") String applicationId) {
+        String executionId = ((OperationService) service).executeJob(corpusId,applicationId);
+        return ResponseEntity.ok(executionId);
+    }
+
 }
