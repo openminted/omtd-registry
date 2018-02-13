ed4f26aec59006631bfd30fe6179f5d6cf0f8c06
Kostas <spyroukon@gmail.com>
2018-02-13 13:11:04 +0200

    added CorpusContentController

---
+++ src/main/java/eu/openminted/registry/service/CorpusContentController.java
@@ -0,0 +1,30 @@
+package eu.openminted.registry.service;
+
+
+import eu.openminted.registry.domain.CorpusContent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by spyroukostas on 7-Feb-2018.
+ */
+@RestController
+@RequestMapping("/request/corpuscontent")
+public class CorpusContentController {
+
+    final private CorpusContentService ccService;
+
+    @Autowired
+    CorpusContentController(CorpusContentService content) {
+        this.ccService = content;
+    }
+
+    @RequestMapping(path = "/getCorpusContent", method = RequestMethod.GET)
+    public CorpusContent getCorpusContent(@RequestParam(value="corpusId") String corpusId) {
+        return ccService.getCorpusContent(corpusId);
+    }
+}
