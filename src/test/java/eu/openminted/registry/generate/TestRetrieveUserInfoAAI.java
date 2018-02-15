a73c40321ffde32dddeff0494b5e93b29bac36fd
Stefanos Gatsios <stevengatsios@gmail.com>
2018-02-15 17:08:44 +0200

    Changing test so it builds

--- src/test/java/eu/openminted/registry/generate/TestRetrieveUserInfoAAI.java
+++ src/test/java/eu/openminted/registry/generate/TestRetrieveUserInfoAAI.java
@@ -1,16 +1,14 @@
 package eu.openminted.registry.generate;
 
 
-import java.io.IOException;
-
-
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import eu.openminted.registry.service.aai.UserInfoAAIRetrieve;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-
+import java.io.IOException;
 
 
 //@ActiveProfiles("test")
@@ -19,30 +17,26 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 //@PropertySource("classpath:/test.properties")
 //@ContextConfiguration(classes=RestTemplateBeanConfig.class)//, loader=AnnotationConfigContextLoader.class)
 public class TestRetrieveUserInfoAAI {
-	static final Logger logger = Logger.getLogger(TestRetrieveUserInfoAAI.class.getName());
+    static final Logger logger = Logger.getLogger(TestRetrieveUserInfoAAI.class.getName());
 
-	@Autowired
-	private UserInfoAAIRetrieve aaiRetriever;
-	
-	//@Test
-	public void testBasic() throws JsonParseException, JsonMappingException, IOException {
-		
-	
-		logger.info("Running Retrieving User info");
-		String userId = "0931731143127784@openminted.eu"; 
+    @Autowired
+    private UserInfoAAIRetrieve aaiRetriever;
 
-		int coId = aaiRetriever.getCoId(userId);
+    //@Test
+    public void testBasic() throws JsonParseException, JsonMappingException, IOException {
+
+        logger.info("Running Retrieving User info");
+        String userId = "0931731143127784@openminted.eu";
+
+        int coId = aaiRetriever.getCoId(userId);
         logger.info("User has coId " + coId);
-	
+
         String email = aaiRetriever.getEmail(coId);
         logger.info("User has email " + email);
-        
-        String surname = aaiRetriever.getSurname(coId);
-        logger.info("User has surname " + surname);
 
-        String givenname = aaiRetriever.getGivenName(coId);
-        logger.info("User has name " + givenname);
+        Pair name = aaiRetriever.getSurnameGivenName(coId);
+        logger.info("User has surname " + name.getLeft());
+        logger.info("User has name " + name.getRight());
 
-      
-	}
+    }
 }
