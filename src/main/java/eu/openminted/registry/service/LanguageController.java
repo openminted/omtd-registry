1eed15e67ff423dc01240a4a628c289f17e8af83
Stefanos Gatsios <stevengatsios@gmail.com>
2018-03-23 20:05:45 +0200

    Ancillary Controllers update to include zipUpload

--- src/main/java/eu/openminted/registry/service/LanguageController.java
+++ src/main/java/eu/openminted/registry/service/LanguageController.java
@@ -2,8 +2,15 @@ package eu.openminted.registry.service;
 
 import eu.openminted.registry.domain.LanguageDescription;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * Created by stefanos on 13/1/2017.
@@ -15,5 +22,16 @@ public class LanguageController extends GenericRestController<LanguageDescriptio
     @Autowired
     LanguageController(ValidateInterface<LanguageDescription> service) {
         super(service);
+    }
+
+    @Autowired
+    private StoreService storeService;
+
+    @RequestMapping(value = "zipUpload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<LanguageDescription> uploadCorpus(@RequestPart("file") MultipartFile file,
+                                               @RequestPart("language") LanguageDescription language) throws IOException {
+        String archiveId = storeService.uploadCorpus(file.getName(), file.getInputStream());
+        LanguageDescription corpusRet = ((AncillaryService<LanguageDescription>) service).uploadZip(language,archiveId);
+        return ResponseEntity.ok(corpusRet);
     }
 } 
--- src/main/java/eu/openminted/registry/service/LexicalController.java
+++ src/main/java/eu/openminted/registry/service/LexicalController.java
@@ -1,9 +1,17 @@
 package eu.openminted.registry.service;
 
+import eu.openminted.registry.domain.LanguageDescription;
 import eu.openminted.registry.domain.Lexical;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * Created by stefanos on 13/1/2017.
@@ -15,5 +23,16 @@ public class LexicalController extends OmtdRestController<Lexical> {
     @Autowired
     LexicalController(ValidateInterface<Lexical> service) {
         super(service);
+    }
+
+    @Autowired
+    private StoreService storeService;
+
+    @RequestMapping(value = "zipUpload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Lexical> uploadCorpus(@RequestPart("file") MultipartFile file,
+                                                            @RequestPart("lexical") Lexical lexical) throws IOException {
+        String archiveId = storeService.uploadCorpus(file.getName(), file.getInputStream());
+        Lexical corpusRet = ((AncillaryService<Lexical>) service).uploadZip(lexical,archiveId);
+        return ResponseEntity.ok(corpusRet);
     }
 } 
