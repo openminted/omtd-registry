3b652801b8634bfa880f5b6e09fc9453636603e4
John Diplas <Jdiplas@gmail.com>
2018-05-07 17:04:00 +0300

    Endpoint for stats

--- src/main/java/eu/openminted/registry/service/tools/StatsController.java
+++ src/main/java/eu/openminted/registry/service/tools/StatsController.java
@@ -1,7 +1,7 @@
 package eu.openminted.registry.service.tools;
 
-import eu.openminted.registry.core.domain.FacetFilter;
-import eu.openminted.registry.core.service.SearchService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import eu.openminted.registry.service.StatsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,30 +11,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-
 @RestController
 public class StatsController {
 
     @Autowired
-    SearchService searchService;
+    StatsService statsService;
 
-    @RequestMapping(value = "/stats/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/stats/totals", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
     public ResponseEntity stats() {
-
-        int applicationNo = 0;
-        int componentNo = 0;
-
-        try {
-            FacetFilter filter = new FacetFilter("","application",0,1,new HashMap<>(), Arrays.asList(""), null);
-            applicationNo = searchService.search(filter).getTotal();
-            filter.setResourceType("component");
-            componentNo = searchService.search(filter).getTotal();
-            return new ResponseEntity<>("{'applications':"+applicationNo + ",'components':"+componentNo+"}", HttpStatus.OK);
-        } catch (IOException e) {
+        try{
+            return new ResponseEntity<>(new ObjectMapper().writeValueAsString(statsService.totals()),HttpStatus.OK);
+        } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
