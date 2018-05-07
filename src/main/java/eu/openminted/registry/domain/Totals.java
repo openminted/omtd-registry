f65e8db6157d1f302e9fc032c60bc10d626fc5b6
John Diplas <Jdiplas@gmail.com>
2018-05-07 17:04:44 +0300

    StatsServiceImpl and CacheConfiguration

---
+++ src/main/java/eu/openminted/registry/domain/Totals.java
@@ -0,0 +1,40 @@
+package eu.openminted.registry.domain;
+
+public class Totals {
+
+    private int publications = 0;
+
+    private int components = 0;
+
+    private int applications = 0;
+
+    public Totals(int publications, int components, int applications) {
+        this.publications = publications;
+        this.components = components;
+        this.applications = applications;
+    }
+
+    public int getPublications() {
+        return publications;
+    }
+
+    public void setPublications(int publications) {
+        this.publications = publications;
+    }
+
+    public int getComponents() {
+        return components;
+    }
+
+    public void setComponents(int components) {
+        this.components = components;
+    }
+
+    public int getApplications() {
+        return applications;
+    }
+
+    public void setApplications(int applications) {
+        this.applications = applications;
+    }
+}
