fc13848cb4169fb1155d2dcb3040a1097c43ebd4
John Diplas <Jdiplas@gmail.com>
2018-05-08 13:16:10 +0300

    Now caching the result of stats/totals

--- src/main/java/eu/openminted/registry/service/tools/StatsController.java
+++ src/main/java/eu/openminted/registry/service/tools/StatsController.java
@@ -1,6 +1,5 @@
 package eu.openminted.registry.service.tools;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.openminted.registry.service.StatsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,7 +7,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,11 +16,11 @@ public class StatsController {
     StatsService statsService;
 
     @RequestMapping(value = "/stats/totals", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    @ResponseBody
     public ResponseEntity stats() {
         try{
-            return new ResponseEntity<>(new ObjectMapper().writeValueAsString(statsService.totals()),HttpStatus.OK);
+            return new ResponseEntity<>(statsService.totals(),HttpStatus.OK);
         } catch (Exception e) {
+            e.printStackTrace();
             return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
