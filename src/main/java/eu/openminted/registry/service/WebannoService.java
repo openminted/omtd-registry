f7435c5cfe8f88d1065ea83b32f1cb7ca525373b
John Diplas <Jdiplas@gmail.com>
2018-05-15 13:52:45 +0300

    Commiting the webannoService interface

---
+++ src/main/java/eu/openminted/registry/service/WebannoService.java
@@ -0,0 +1,17 @@
+package eu.openminted.registry.service;
+
+/**
+ * Created by stefanos on 3/12/18.
+ */
+public interface WebannoService {
+
+    /**
+     * Method for uploading a corpus, at the webanno
+     * annotation viewer, as a project.
+     * @param corpusId
+     */
+    boolean createProject(String corpusId);
+
+    void deleteProject(String corpusId);
+
+}
