9597a343f0ea0caa74f913d360e63640d2be2aca
John Diplas <Jdiplas@gmail.com>
2018-05-15 13:53:51 +0300

    Commiting Webanno controller

---
+++ src/main/java/eu/openminted/registry/service/tools/WebannoController.java
@@ -0,0 +1,29 @@
+package eu.openminted.registry.service.tools;
+
+import eu.openminted.registry.service.WebannoService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebannoController {
+
+    private Logger logger = LogManager.getLogger(WebannoController.class);
+
+    @Autowired
+    WebannoService webannoService;
+
+    @RequestMapping(value = "/webanno/done", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity stats() {
+            logger.info("Hello there!");
+            return new ResponseEntity<>("hello",HttpStatus.OK);
+
+    }
+
+}
