7f280e98003a72abb493606a35d4dd10501ff865
John Diplas <Jdiplas@gmail.com>
2018-05-16 13:24:13 +0300

    Testing webanno

--- src/main/java/eu/openminted/registry/service/tool/WebannoServiceImpl.java
+++ src/main/java/eu/openminted/registry/service/tool/WebannoServiceImpl.java
@@ -9,6 +9,7 @@ import org.json.JSONObject;
 import org.postgresql.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -17,6 +18,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.File;
 
 
 @Service("webannoService")
@@ -47,15 +50,40 @@ public class WebannoServiceImpl implements WebannoService{
 
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
         ResponseEntity response = restTemplate.postForEntity(webannoHost+"/projects",request, String.class);
-        if(response.getStatusCode().equals("201")) {
-            JSONObject jsonObject = (JSONObject) new JSONObject(response.getBody().toString()).getJSONObject("body");
-            logger.error(jsonObject.getInt("id"));
+        logger.error(response.getStatusCode());
+        if(response.getStatusCode().value()==201) {
+            int projectId = new JSONObject(response.getBody().toString()).getJSONObject("body").getInt("id");
+
+//            for(File file : listFiles()){
+//                uploadDocument(projectId, file);
+//            }
+
+
             return true;
         }else {
             logger.debug(webannoHost + " returned " + response.getStatusCode() + " | Logger level ERROR for more info");
             logger.error(response.getBody().toString());
             return false;
         }
+    }
+
+    private void uploadDocument(int projectId, File file){
+        MultiValueMap<String, Object> map= new LinkedMultiValueMap<String, Object>();
+        map.add("name", file.getName());
+        map.add("format", "XMI");
+        map.add("content", new FileSystemResource(file));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization","Basic "+basicAuth());
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(map, headers);
+        ResponseEntity response = restTemplate.postForEntity(webannoHost+"/projects/"+String.valueOf(projectId)+"/documents",request, String.class);
+
+        if(response.getStatusCode().value()!=201) {
+            logger.error("Could not upload " + file.getName()+ " @ Webanno. Moving on to the next one");
+        }
+
     }
 
     private void deleteProject(String corpusId) {
@@ -72,7 +100,19 @@ public class WebannoServiceImpl implements WebannoService{
 
     @Override
     public void triggerRetrieval(long projectId) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization","Basic "+basicAuth());
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 
+        HttpEntity request = new HttpEntity<>(headers);
+        ResponseEntity response = restTemplate.postForEntity(webannoHost+"/projects/"+Math.toIntExact(projectId)+"/export.zip",request, String.class);
+        logger.error(response.getStatusCode());
+        if(response.getStatusCode().value()==201) {
+            logger.error(response.getBody().toString());
+        }else {
+            logger.debug(webannoHost + " returned " + response.getStatusCode() + " | Logger level ERROR for more info");
+            logger.error(response.getBody().toString());
+        }
 
 
 
