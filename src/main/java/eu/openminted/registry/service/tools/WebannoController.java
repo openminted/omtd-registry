6c2294500204f9646fd5200e86436d466b0e1e8a
John Diplas <Jdiplas@gmail.com>
2018-05-18 15:32:30 +0300

    Testing webanno

--- src/main/java/eu/openminted/registry/service/tools/WebannoController.java
+++ src/main/java/eu/openminted/registry/service/tools/WebannoController.java
@@ -1,16 +1,15 @@
 package eu.openminted.registry.service.tools;
 
 import eu.openminted.registry.service.WebannoService;
-import eu.openminted.registry.service.requests.DocumentStateChangeMessage;
+import eu.openminted.registry.service.requests.ProjectStateChangeMessage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletContext;
 
 @RestController
 public class WebannoController {
@@ -18,19 +17,13 @@ public class WebannoController {
     private Logger logger = LogManager.getLogger(WebannoController.class);
 
     @Autowired
+    ServletContext context;
+
+    @Autowired
     WebannoService webannoService;
 
-    @RequestMapping(value = "/webanno/done", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity done_POST (DocumentStateChangeMessage documentStateChangeMessage) {
-           webannoService.triggerRetrieval(documentStateChangeMessage.getProjectId());
-           return new ResponseEntity<>("hello",HttpStatus.OK);
+    @RequestMapping(value = "/webanno/done", method = RequestMethod.POST)
+    public void done_POST (ProjectStateChangeMessage projectStateChangeMessage) {
+        webannoService.triggerRetrieval(projectStateChangeMessage.getProjectId(), projectStateChangeMessage.getProjectName());
     }
-
-//    @RequestMapping(value = "/webanno/done", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-//    public ResponseEntity done_POST_doc (DocumentStateChangeMessage documentStateChangeMessage) {
-//        webannoService.triggerRetrieval(documentStateChangeMessage.getProjectId());
-//        return new ResponseEntity<>("hello",HttpStatus.OK);
-//    }
-
-
 }
