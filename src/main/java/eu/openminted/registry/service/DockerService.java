9be0a402b0b3041c71fda1c8ccf20a0f08a90a56
John Diplas <Jdiplas@gmail.com>
2018-06-15 13:07:50 +0300

    Removed unused dockerService functions

--- src/main/java/eu/openminted/registry/service/DockerService.java
+++ src/main/java/eu/openminted/registry/service/DockerService.java
@@ -1,17 +1,9 @@
 package eu.openminted.registry.service;
 
-import eu.openminted.registry.core.service.ResourceCRUDService;
-import eu.openminted.registry.domain.Corpus;
-
-import java.io.InputStream;
-
 
 public interface DockerService {
 
-    String uploadDockerFlow(String url);
 
     void downloadDockerFlow(String url);
-
-    void deleteDockerFlow(String url, String image_id);
 
 }
